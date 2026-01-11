0763f042edc7296e2408693a868653c8eb66201f
Genxster <ck.2229.ck@gmail.com>
2026-01-11 22:56:50 +0530

    Deleted redundant files

--- debug_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running PixelDrain application with debug output..."
-echo "Please click on the Lists tab and then click 'Refresh Lists' to see the debug output"
-echo "Press Ctrl+C to stop the application"
-echo "----------------------------------------"
-
-cargo run 2>&1 | tee debug_output.log 
